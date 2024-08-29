05682d229955841c09294889886228b2c36fafbd
cancer-oncogenomics <82413295+cancer-oncogenomics@users.noreply.github.com>
2024-08-29 14:47:30 -0400

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,6 +1,10 @@
-# Model Training
-./main LungModel         --feature  tests/feature/MS.csv         --train_info  tests/Train_info.list         --pred_info  tests/Valid_info.list         --d_output  layer1/MS/         --prefix  MS         --nthreads   10
-./main LungModel         --feature  tests/feature/CNV.csv         --train_info  tests/Train_info.list         --pred_info  tests/Valid_info.list         --d_output  layer1/CNV/         --prefix  CNV         --nthreads   10
-./main LungModel        --feature  tests/feature/FSD.csv         --train_info  tests/Train_info.list         --pred_info  tests/Valid_info.list         --d_output  layer1/FSD/         --prefix  FSD         --nthreads   10
+# Layer 1: Model Training
+./main.py LungModel         --feature  tests/feature/MS.csv         --train_info  tests/Train_info.list         --pred_info  tests/Valid_info.list         --d_output  layer1/MS/         --prefix  MS         --nthreads   10
+./main.py LungModel         --feature  tests/feature/CNV.csv         --train_info  tests/Train_info.list         --pred_info  tests/Valid_info.list         --d_output  layer1/CNV/         --prefix  CNV         --nthreads   10
+./main.py LungModel        --feature  tests/feature/FSD.csv         --train_info  tests/Train_info.list         --pred_info  tests/Valid_info.list         --d_output  layer1/FSD/         --prefix  FSD         --nthreads   10
 
 # Base Model ROC Calculation
+Rscript ./basemodel.roc.cal.R --info tests/ --automl_dir layer1/ --fout basemodel.auc.summary.txt
+
+# Layer 2
+Rscript ./featureCombn.R --top_n 5 --base_auc_dir layer1/
